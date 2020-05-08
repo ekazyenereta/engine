6f484471c937f546ea38dae6439b70968662c703
JayceLai <goodlaijiaxin@qq.com>
2020-05-08 14:47:39 +0800

    add CC_CONFIG to ammo\cannon

--- cocos/physics/ammo/ammo-rigid-body.ts
+++ cocos/physics/ammo/ammo-rigid-body.ts
@@ -34,8 +34,13 @@ export class AmmoRigidBody implements IRigidBody {
         // See https://studiofreya.com/game-maker/bullet-physics/bullet-physics-how-to-change-body-mass/
         const localInertia = this._sharedBody.bodyStruct.localInertia;
         localInertia.setValue(1.6666666269302368, 1.6666666269302368, 1.6666666269302368);
-        if (this._btCompoundShape.getNumChildShapes() > 0) {
-            this._btCompoundShape.calculateLocalInertia(this._rigidBody.mass, localInertia);
+        const shape = this.impl.getCollisionShape();
+        if (shape.isCompound()) {
+            if (this._sharedBody.bodyCompoundShape.getNumChildShapes() > 0) {
+                shape.calculateLocalInertia(this._rigidBody.mass, localInertia);
+            }
+        } else {
+            shape.calculateLocalInertia(this._rigidBody.mass, localInertia);
         }
         this.impl.setMassProps(value, localInertia);
         this._sharedBody.updateByReAdd();
@@ -96,7 +101,7 @@ export class AmmoRigidBody implements IRigidBody {
                 this.impl.setActivationState(AmmoCollisionObjectStates.ACTIVE_TAG);
             }
         } else {
-            this.impl.setActivationState(AmmoCollisionObjectStates.DISABLE_DEACTIVATION);
+            this.impl.forceActivationState(AmmoCollisionObjectStates.DISABLE_DEACTIVATION);
         }
     }
 
@@ -110,7 +115,6 @@ export class AmmoRigidBody implements IRigidBody {
     private _isEnabled = false;
     private _sharedBody!: AmmoSharedBody;
     private _rigidBody!: RigidBodyComponent;
-    private get _btCompoundShape () { return this._sharedBody.bodyCompoundShape };
 
     private _btVec3_0 = new Ammo.btVector3();
     private _btVec3_1 = new Ammo.btVector3();
